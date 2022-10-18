fb063537d3cc2d4d905f9ea2075718f647032726
Blaj3n <benceulrich@gmail.com>
2022-10-18 13:18:57 +0200

    sorok felvitele a táblázatba

--- View/KonyvView.js
+++ View/KonyvView.js
@@ -3,6 +3,12 @@ class KonyvView {
         // console.log("KonyvView");
         console.log(elem);
         //egy táblázat sort jelenítsünk meg
+        szuloElem.append(`<tr>
+        <th>${elem.id}</th>
+        <th>${elem.cim}</th>
+        <th>${elem.szerzo}</th>
+        <th>${elem.ar}</th>
+        </tr>`);
     }
 }
 
